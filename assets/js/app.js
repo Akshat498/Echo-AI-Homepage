ff6d1733ae7b13fd971843acc1e4370fa1d673b6
akshat498 <akshatdhukia23@gmail.com>
2025-01-06 15:46:34 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+$('.Testimonials').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    autoplay:true,
+    arrows: false,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+
+
+    ]
+});
+
+const navbarbtn = () => {
+    document.getElementById("nav-name").classList.toggle("show-nvabar")
+    document.getElementById("rotats").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
